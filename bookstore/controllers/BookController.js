f5b9e60bfaefd3059abc8ae1f67f1ed1bcf7a0f8
Luiz Carlos <luizlemos02@gmail.com>
2023-09-11 00:20:29 -0300

    atualizando acesso ao bd

--- bookstore/controllers/BookController.js
+++ bookstore/controllers/BookController.js
@@ -1,8 +1,9 @@
 const Book = require('../models/Book');             // IMPORTANDO MODELO DO BOOK
 
 module.exports = class BookController {
-    static showBooks(req, res) {                            // REQUISIÇÃO E RESPOSTA DO MÉTODO
-        res.render('books/all');                            // RENDERIZAR A VIEW ALL BOOKS
+    static async showBooks(req, res) {                      // REQUISIÇÃO E RESPOSTA DO MÉTODO
+        const books = await Book.getBooks();                // BOOKS ESPERA A CONSULTA AO BANCO
+        res.render('books/all', {books});                   // RENDERIZAR PASSANDO DADOS DE BOOKS PARA VIEW
     };
 
     static addBook(req, res) {
@@ -11,10 +12,11 @@ module.exports = class BookController {
 
     static addBookPost(req, res) {
         const name = req.body.name;                         // PEGANDO DADOS DO BODY PARA ESCREVER NO BANCO
+        const image = req.body.image;
         const price = req.body.price;
         const description = req.body.description;
         
-        const book = new Book(name, price, description);    // INSTÂNCIA DE BOOK COM OS DADOS NA ORDEM DO CONSTRUTOR
+        const book = new Book(name, image, price, description);    // INSTÂNCIA DE BOOK COM OS DADOS NA ORDEM DO CONSTRUTOR
 
         book.save();                                        // SALVAR OS DADOS DO BOOK NO BANCO PELO MÉTODO SAVE
 
--- bookstore/models/Book.js
+++ bookstore/models/Book.js
@@ -1,8 +1,9 @@
 const conn = require('../db/conn');                         // ADICIONA MÓDULO CONEXÃO EM MONGODB
 
 class Book {                                                // CRIANDO CLASSE BOOK
-    constructor(name, price, description) {                 // CONSTRUTOR COM PROPRIEDADES PREENCHIDAS
+    constructor(name, image, price, description) {                 // CONSTRUTOR COM PROPRIEDADES PREENCHIDAS
         this.name = name;
+        this.image = image;
         this.price = price;
         this.description = description;
     };
@@ -10,11 +11,18 @@ class Book {                                                // CRIANDO CLASSE BO
     save() {                                                            // MÉTODO PARA OPERAR COM O BANCO DE DADOS
         const book = conn.db().collection('bookstore').insertOne({      // INFORMA BANCO E COLLECTION PARA INSERÇÃO
             name: this.name,                                            // SE NÃO EXISTIR, SERÁ CRIADO
+            image: this.image,
             price: this.price,
             description: this.description
         });
     
         return book;
+    };
+
+    static getBooks() {
+        const books = conn.db().collection('bookstore').find().toArray();      // ACESSA O BANCO E CONVERTE O DADO PARA ARRAY
+    
+        return books;
     };
 };
 
